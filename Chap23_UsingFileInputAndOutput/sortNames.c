e23a89bf1ed734eaedfe260de1ac7942042127e0
skelter 2014 <skelter2014gm@gmail.com>
2025-01-17 06:27:56 -0500

    .

--- Chap23_UsingFileInputAndOutput/sortNames.c
+++ Chap23_UsingFileInputAndOutput/sortNames.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> //for getopt
-#include <sys/errno.h> //for errno
+#include <unistd.h> // for getopt
+#include <sys/errno.h> // for errno
 #include <ctype.h>
 #include "nameList.h"
 
@@ -22,27 +22,26 @@ int getName(FILE* inFileDesc, char* pStr);
 void putName(char* nameStr, FILE* outFileDesc);
 int trimStr(char* pString);
 
-//**main goes here
-int main(int argc, char* argv[])
-{
+//**main function
+int main(int argc, char* argv[]) {
     int ch;
     FILE* inputFile = NULL;
     FILE* outputFile = NULL;
 
+    // Parse command-line arguments
     while ((ch = getopt(argc, argv, "i:o:h")) != -1) {
         switch (ch) {
             case 'i':
                 if (NULL == (inputFile = fopen(optarg, "r"))) {
-                    fprintf(stderr, "input file\"%s\": %s\n", optarg, strerror(errno));
+                    fprintf(stderr, "input file \"%s\": %s\n", optarg, strerror(errno));
                     exit(EXIT_FAILURE);
                 }
-                fprintf(stderr, "Using \"%s\" for input. \n", optarg);
+                fprintf(stderr, "Using \"%s\" for input.\n", optarg);
                 break;
             case 'o':
                 if (NULL == (outputFile = fopen(optarg, "w"))) {
                     fprintf(stderr, "output file \"%s\": %s\n", optarg, strerror(errno));
                     exit(EXIT_FAILURE);
-
                 }
                 fprintf(stdout, "Using \"%s\" for output.\n", optarg);
                 break;
@@ -53,6 +52,8 @@ int main(int argc, char* argv[])
                 break;
         }
     }
+
+    // Default to stdin and stdout if no files are provided
     if (!inputFile) {
         inputFile = stdin;
         fprintf(stdout, "Using stdin for input.\n");
@@ -62,6 +63,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "Using stdout for output.\n");
     }
 
+    // Process names
     char nameBuffer[stringMax];
     NameList nameList = { 0 };
     while (getName(inputFile, nameBuffer)) {
@@ -70,55 +72,56 @@ int main(int argc, char* argv[])
     PrintNames(outputFile, &nameList);
     DeleteNames(&nameList);
 
+    // Close files
     fprintf(stdout, "Closing files.\n");
     fclose(inputFile);
     fflush(outputFile);
     fclose(outputFile);
 }
-//**function defs
-void usage(char* cmd)
-{
+
+//**function definitions
+
+// Display usage information
+void usage(char* cmd) {
     fprintf(stderr, "usage: %s [-i inputFName] [-o outputFName]\n", cmd);
     fprintf(stderr, " if -i inputFName is not given stdin is used.\n");
-    fprintf(stderr, " if -o outputFname is not given stdout is used.\n\n");
-
+    fprintf(stderr, " if -o outputFName is not given stdout is used.\n\n");
     exit(EXIT_FAILURE);
 }
-int trimStr(char* pStr)
-{
+
+// Trim leading and trailing whitespace from a string
+int trimStr(char* pStr) {
     size_t first, last, lenIn, lenOut;
     first = last = lenIn = lenOut = 0;
 
     lenIn = strlen(pStr);
-    char tmpStr[lenIn + 1]; //create a working copy
+    char tmpStr[lenIn + 1]; // create a working copy
     strcpy(tmpStr, pStr);
-    char* pTmp = tmpStr; //pTmp may change in left trim
+    char* pTmp = tmpStr; // pTmp may change in left trim
 
-    //Left Trim
-    //Find 1st non-whitespace char. pStr will point to that
+    // Left Trim
+    // Find 1st non-whitespace char. pStr will point to that
     while (isspace(pTmp[first])) { first++; }
     pTmp += first;
     lenOut = strlen(pTmp);
 
-    if (lenOut) { //only trim right if len > 0
-        //Right Trim
-        //Find 1st non-whitespace char and set NUL char there
+    if (lenOut) { // only trim right if len > 0
+        // Right Trim
+        // Find 1st non-whitespace char and set NUL char there
         last = lenOut - 1;
         while (isspace(pTmp[last])) { last--; }
         pTmp[last + 1] = '\0';
 
         lenOut = strlen(pTmp);
-        if (lenIn != lenOut) {   // did anything change:?
-            strcpy(pStr, pTmp); //yes. copy trimmed string back to pStr (passed in by ref)
+        if (lenIn != lenOut) { // did anything change?
+            strcpy(pStr, pTmp); // yes. copy trimmed string back to pStr (passed in by ref)
         }
     }
     return lenOut;
-
-
-
 }
-int getName(FILE* inFileDesc, char* pStr)
-{
+
+// Get a name from the input file
+int getName(FILE* inFileDesc, char* pStr) {
     static int numNames = 0;
     int len;
 
@@ -131,8 +134,9 @@ int getName(FILE* inFileDesc, char* pStr)
     if (len) numNames++;
     return len;
 }
-void putName(char* pStr, FILE* outFileDesc)
-{
+
+// Write a name to the output file
+void putName(char* pStr, FILE* outFileDesc) {
     fputs(pStr, outFileDesc);
     fputc('\n', outFileDesc);
 }
