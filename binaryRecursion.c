697ec1c54d9536e62b1b86b4d5e32a61fa8d9a9c
skelter 2014 <skelter2014gm@gmail.com>
2025-01-17 04:32:59 -0500

    .

--- binaryRecursion.c
+++ binaryRecursion.c
@@ -10,13 +10,27 @@ const char indentChar = ' ';
 This program uses binary recursion to add all of the numbers in an array. It splits the array in half
 and calls the function recursively on each half until there is only one element in the array, at which
 point the value can be derived.
+
+static const char to_red[] = "\033...";
+static const char to_black[] = "\033...";
+
+printf("%s%s%s\n", to_red, "hello world", to_black)
 */
+
+//Ansi color codes -> \033 is the escape sequence
+static  char to_red[] = "\033[31m";
+static const char to_blue[] = "\033[34m";
+static  char to_yellow[] = "\033[33m";
+static  char to_default[] = "\033[0m";
+
 
 int main(void)
 {
 
-    int data[] = { 1,1,1,1,1,1,1,1,1,1 };
-    printf("\nThe sum of all the digits in the array: %d\n\n", binarySum(data, 0, 9, "-") );
+
+
+    int data[] = { 10,20,30,40,50,60,70,80,90,100 };
+    printf("\nThe sum of all the digits in the array: %d\n\n", binarySum(data, 0, 9, "-"));
     printf("\n");
 
 
@@ -24,9 +38,11 @@ int main(void)
 
 int binarySum(int* data, int low, int high, char* src)
 {
+     const char * color = to_blue;
     static int count = 0;
-    indent(count,indentChar);
-    printf("low: %d  high: %d   src: %s\n", low, high, src);
+    if (src == "left") { color = to_yellow; }
+    indent(count, indentChar);
+    printf("low: %d  high: %d   src: %s%s%s\n", low, high, color, src, to_default);
 
     if (low > high)
     {
@@ -34,8 +50,8 @@ int binarySum(int* data, int low, int high, char* src)
     }
 
     else if (low == high) {
-        indent(count,indentChar);
-        printf("data[%d]=%d\n", low, data[low]);
+        indent(count, indentChar);
+        printf("%sdata[%d]=%d%s\n", to_red, low, data[low], to_default);
         count--;
         return data[low];
     }
@@ -46,7 +62,8 @@ int binarySum(int* data, int low, int high, char* src)
     }
 }
 
-void indent(int count, char c){
-  for (int i=0; i < count*4; i++) printf("%c",c);
+void indent(int count, char c)
+{
+    for (int i = 0; i < count * 4; i++) printf("%c", c);
 }
 
